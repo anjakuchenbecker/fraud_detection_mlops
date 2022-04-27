84fceb197fa9b549f285756beb7f5f0e3feb3506
Anja Kuchenbecker <kuchenbecker@gmail.com>
2022-04-27 22:11:53 +0000

    enhanced train.py with argparse and mlflow tracking uri

--- 6_implement/model_training/train.py
+++ 6_implement/model_training/train.py
@@ -1,3 +1,27 @@
 # TODO: develop training module
 
-print("training module: hello world")
+# External imports
+import mlflow
+import argparse
+# Internal imports
+# NA
+
+def setup():
+    """
+    Setup the training module
+    """
+
+    # Setup the command-line parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mlflow_tracking_uri", 
+                        help="Set the MLflow tracking server URI", 
+                        type=str)
+    args = parser.parse_args()
+    
+    # Set tracking uri (tracking server and registry server are the same / not separated)
+    # No need to set the registry uri in addition, because it defaults to the tracking URI
+    mlflow.set_tracking_uri(args.mlflow_tracking_uri)
+
+if __name__ == "__main__":
+    setup()
+    print("training module: hello world")
