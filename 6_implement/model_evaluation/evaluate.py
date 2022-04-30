6df43d28a436abd652fb712055f5e8495410e773
Anja Kuchenbecker <kuchenbecker@gmail.com>
2022-04-30 06:11:15 +0200

    added first interations with MLFlow tracking server in train and evaluation module

--- 6_implement/model_evaluation/evaluate.py
+++ 6_implement/model_evaluation/evaluate.py
@@ -1,3 +1,52 @@
 # TODO: develop evaluation module
 
-print("evaluation module: hello world")
+# External imports
+import mlflow
+import argparse
+# Internal imports
+# NA
+
+
+def setup():
+    """
+    Setup the evaluation module
+    """
+
+    # Setup the command-line parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mlflow_tracking_uri",
+                        help="Set the MLflow tracking server URI",
+                        type=str)
+    parser.add_argument("mlflow_run_id",
+                        help="Set the MLflow run id",
+                        type=str)
+    args = parser.parse_args()
+
+    # Set tracking uri (tracking server and registry server are the same / not separated)
+    # No need to set the registry uri in addition, because it defaults to the tracking URI
+    mlflow.set_tracking_uri(args.mlflow_tracking_uri)
+    # Get details from given run
+    run = mlflow.get_run(args.mlflow_run_id)
+    return run
+
+
+def load_metric_expections():
+    """
+    TODO
+    """
+
+    # TODO
+    pass
+
+
+def evaluate_model():
+    """
+    TODO
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    run = setup()
+    print(run.data.params)
--- 6_implement/model_training/train.py
+++ 6_implement/model_training/train.py
@@ -6,6 +6,7 @@ import argparse
 # Internal imports
 # NA
 
+
 def setup():
     """
     Setup the training module
@@ -13,15 +14,75 @@ def setup():
 
     # Setup the command-line parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("mlflow_tracking_uri", 
-                        help="Set the MLflow tracking server URI", 
+    parser.add_argument("mlflow_tracking_uri",
+                        help="Set the MLflow tracking server URI",
+                        type=str)
+    parser.add_argument("mlflow_experiment_name",
+                        help="Set the name of the MLflow experiment",
                         type=str)
     args = parser.parse_args()
-    
+
     # Set tracking uri (tracking server and registry server are the same / not separated)
     # No need to set the registry uri in addition, because it defaults to the tracking URI
     mlflow.set_tracking_uri(args.mlflow_tracking_uri)
+    # Set experiment
+    experiment = mlflow.set_experiment(args.mlflow_experiment_name)
+    return experiment.experiment_id
+
+
+def load_processed_data():
+    """
+    TODO
+    """
+
+    # TODO processed_train.<file>, processed_test.<file>
+    # return train_X, train_y, test_X, test_y
+    pass
+
+
+def calculate_eval_metrics(actual, predicted):
+    """
+    TODO
+    """
+
+    # TODO
+    # Mean squared error regression loss
+    # rmse = mean_squared_error(y_true=actual, y_pred=predicted, squared=False)
+    # Mean absolute error regression loss
+    # mae = mean_absolute_error(actual, predicted)
+    # r2 = r2_score(actual, predicted)
+    # return rmse, mae, r2
+    pass
+
+
+def train():
+    """
+    TODO
+    """
+
+    # train_X, train_y, test_X, test_y
+    model_params = {"Pa_Dummy_1": 1, "Pa_Dummy_2": 2}
+    # model_name = "TBD"
+
+    with mlflow.start_run() as run:
+        # define and fit model with params on processed training dataset
+        # TODO .fit(train_X, train_y)
+
+        # predict on processed test dataset
+        # TODO predicted = (test_X)
+
+        # calculate evaluation metrics
+        # TODO eval_metrics(test_y, predicted)
+
+        # log experiment
+        mlflow.log_params(model_params)
+        # mlflow.log_metrics("TBD", TBD)
+        # mlflow.log_artifacts("TBD", TBD)
+        # mlflow.sklearn.log_model("TBD", TBD)
+        return run.info.run_id
+
 
 if __name__ == "__main__":
     setup()
-    print("training module: hello world")
+    run_id = train()
+    print(run_id)
